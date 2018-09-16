134c8741ca47ca882b9bf7edae24f92c2b3b395d
Stu Btwotwo <stub22@appstract.com>
2018-09-16 22:03:18 +0800

    Skeleton for new Midi experiments, plus beginning of a "survey" docs tree.

---
+++ glungmvn/midi_fun/src/main/scala/mfun_trial/MfunHowdy.scala
@@ -0,0 +1,8 @@
+package org.friendularity.glung.mfun.trial
+
+object MidiFunStuff {
+	def main(args : Array[String]) {
+		println("MidiFunStuff says: Hello World!")
+		println("concat arguments = " + args)
+	}
+}
